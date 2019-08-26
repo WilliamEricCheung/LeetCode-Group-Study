87d25b4bd0d8dbf0a65d4e62dc48cd766e8d0608
FerdinandFan <yf1399@nyu.edu>
2019-08-26 11:12:43 -0400

    yf_commit

--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -2,5 +2,6 @@ package LeetCode;
 public class Main{
     public static void main(String[] args) {
         System.out.println("hello, world");
+        System.out.print("yf_lcgroup_branch");
     }
 }

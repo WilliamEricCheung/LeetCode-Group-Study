92942b3b3a6bc6568c37f90d7ba5fb76542d67bb
ningzhy3 <zn414@nyu.edu>
2019-08-29 19:30:50 -0400

    leetcode_217(hashset)

---
+++ NewLeetCode/src/zhiyuan/solution/Main.java
@@ -0,0 +1,22 @@
+package zhiyuan.solution;
+
+/**
+ * ClassName:Main
+ * PackgeName:zhiyuan.solution
+ * Description:
+ *
+ * @Date:2019-08-29 19:13
+ * Author:ningzhy3@gmail.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int nums [] = new int[] {1,2,3,4,1};
+
+        ContainsDuplicate_217 result = new ContainsDuplicate_217();
+        boolean results = result.containsDuplicate(nums);
+        System.out.println(results);
+    }
+
+}

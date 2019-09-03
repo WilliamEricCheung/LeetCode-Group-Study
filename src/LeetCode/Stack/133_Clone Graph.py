d95477a7e35c4de8e9b3de24b4d225fba829e98a
chensifandz <chensifandz0@gamil.com>
2019-09-02 21:05:45 -0400

    133

---
+++ src/LeetCode/Stack/133_Clone Graph.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, neighbors):
+        self.val = val
+        self.neighbors = neighbors
+"""
+class Solution:
+    def cloneGraph(self, node):
+          def dfs(node,reach):
+            if node in reach:
+                return reach[node]
+            new_node=Node(node.val,[])
+            reach[node]=new_node
+            for new_neighbors in node.neighbors:
+                new_node.neighbors.append(dfs(new_neighbors,reach))
+            return new_node
+          if node== None:
+                    return None
+          return dfs(node,{})

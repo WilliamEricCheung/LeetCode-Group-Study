a97d525097d1b66f6348e6fe3df3eff5a3dfa36c
chensifandz <chensifandz0@gamil.com>
2019-09-04 14:10:37 -0400

    232

---
+++ src/LeetCode/Stack/232_Implement Queue using Stacks.py
@@ -0,0 +1,41 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.stack.pop(0)
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if self.stack:
+            return False
+        return True
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()

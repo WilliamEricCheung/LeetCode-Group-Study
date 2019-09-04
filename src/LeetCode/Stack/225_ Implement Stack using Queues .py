99cc98239acdf1604de252852253d4e06224ceef
chensifandz <chensifandz0@gamil.com>
2019-09-04 16:25:53 -0400

    225

---
+++ src/LeetCode/Stack/225_ Implement Stack using Queues .py
@@ -0,0 +1,33 @@
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.stack.pop()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        if self.stack:
+            return False
+        return True

a78860183753058393f599c7be235212c9cce99f
chensifandz <chensifandz0@gamil.com>
2019-09-07 22:12:53 -0400

    1

---
+++ src/LeetCode/Array/1_two sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        minus=[]
+        print nums
+        for i in range(len(nums)):
+            if nums[i] not in minus:
+                minus.append(target-nums[i])
+            else:
+                return(minus.index(nums[i]),i)

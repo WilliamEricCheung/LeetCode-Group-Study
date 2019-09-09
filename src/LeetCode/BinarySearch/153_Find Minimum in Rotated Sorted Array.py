1337917b0c6147188eaeb32be4c546cf5277a183
chensifandz <chensifandz0@gamil.com>
2019-09-09 13:12:45 -0400

    153

---
+++ src/LeetCode/BinarySearch/153_Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        n = len(nums)
+        right = n - 1
+        while (right > left):
+            mid = (right + left) // 2
+
+            if (nums[mid] >= nums[0] and nums[mid] > nums[mid + 1]):
+                return nums[mid + 1]
+            elif (nums[mid] < nums[0]):
+
+                right = mid
+            else:
+                left = mid + 1
+        if (nums[left] > nums[0]):
+            return nums[0]
+        return nums[left]
